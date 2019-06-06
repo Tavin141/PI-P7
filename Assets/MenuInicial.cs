35ae4e9c26203f509646999daf5b17237acf9334
Ioshua <jose.luiz.f168@gmail.com>
2019-06-06 12:07:59 -0300

    menu quase pronto

---
+++ Assets/MenuInicial.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuInicial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Chamaiapika(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Persona PI");
+    }
+}
